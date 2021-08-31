f4b94eaff24dc38f9556217db647386fb145e4c5
GhitaB <ghitabizau27@gmail.com>
2021-08-31 13:47:27 +0300

    WIP.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+
+function ItemsList(props) {
+  const itemsList = props.items;
+  const items = itemsList.map((item) =>
+    <li key={item.toString()}>
+      TEST: {item}
+    </li>
+  );
+  return (
+    <ul>{items}</ul>
+  );
+}
 
 function App() {
+  const [events, setEvents] = useState(0);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/ghitab/events/public", {
+        method: "GET",
+        headers: {
+          Authorization: `token TOKEN `
+        }
+      })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          debugger;
+          // https://api.github.com/rate_limit
+          var filtered = result.filter((item) => {
+            return item.type === "PushEvent"
+          });
+          var prepared = [];
+          for(var evt of filtered) {
+            prepared.push(evt.id);
+          }
+          // console.log(filtered);
+          setEvents(prepared);
+        }
+      );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>TEST: {events}</p>
+      // <ItemsList items={events} />
     </div>
   );
 }
